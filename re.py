fd86e8e9e47e5c7f8aadde75de64b187a541d699
Kristin Chen <coolfe184770@gmail.com>
2021-08-29 03:10:13 -0500

    verse-2

--- re.py
+++ re.py
@@ -9,6 +9,18 @@ with open('reviews.txt','r') as f:
 print('檔案讀取完成共有',len(data),'筆資料')
 
 
+#計算留言平均長度
+sum = 0
+for d in data: #將data裡面每一個字提出來
+    sum = sum + len(d)
+    if sum % 100000000 == 0:
+        print(len(sum)) #總共有這些數字
+
+print('每筆平均留言長度是',sum/len(data))
+
+
+
+
 #print(len(data)) #印出流言有幾筆
 #print(data[0])#印出清單第一筆 清單索引從0開始
 #print('---------------------------------------------')
